5d35837202095062db6cdc4fc9134fcfd7630717
Egor Marchenko <t469mo@gmail.com>
2021-12-10 12:11:55 +0300

    Model and controller created

---
+++ src/main/java/com/egorkuban/restaurantvote/RestaurantvoteApplication.java
@@ -0,0 +1,13 @@
+package com.egorkuban.restaurantvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantvoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantvoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/egorkuban/restaurantvote/RestaurantvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egorkuban.restaurantvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantvoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
