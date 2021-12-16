68d48a09d7fd2d75df8fdf43830b2ced58e403f6
Egor Marchenko <t469mo@gmail.com>
2021-12-16 11:29:51 +0300

    added methods getAllRestaurants and vote in UserService

--- src/main/java/com/egorkuban/restaurantvote/controller/UserController.java
+++ src/main/java/com/egorkuban/restaurantvote/controller/UserController.java
@@ -1,14 +1,15 @@
 package com.egorkuban.restaurantvote.controller;
 
 import com.egorkuban.restaurantvote.jpa.entity.RestaurantEntity;
+import com.egorkuban.restaurantvote.model.response.VoteResponse;
 import com.egorkuban.restaurantvote.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -17,27 +18,24 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
 
-//    //
-//    //Юзер отправляет запрос - Ответ: список ресторанов
-//    //проверки в отдельный метод потом уберу
-//    @GetMapping("/restaurants")
-//    public ResponseEntity<List<RestaurantEntity>> getAllRestaurants() {
-//        final List<RestaurantEntity> allRestaurantsWithMeals = userService.getAllRestaurants();
-//        return allRestaurantsWithMeals != null && !allRestaurantsWithMeals.isEmpty()
-//                ? new ResponseEntity<>(allRestaurantsWithMeals, HttpStatus.OK)
-//                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//    }
+    //Юзер отправляет запрос - Ответ: список ресторанов
+    @GetMapping("/restaurants")
+    public ResponseEntity<List<RestaurantEntity>> getAllRestaurants() {
+        final List<RestaurantEntity> allRestaurantsWithMeals = userService.getAllRestaurants();
+        return allRestaurantsWithMeals != null && !allRestaurantsWithMeals.isEmpty()
+                ? new ResponseEntity<>(allRestaurantsWithMeals, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 
-//    //Юзер отправляет Id ресторана - Ответ: id ресторана + дата
-//    @PostMapping("/restaurants/{id}/vote")
-//    public ResponseEntity<VoteResponse> vote(@PathVariable Long id) {
-//        //проверки в отдельный метод потом уберу
-//        LocalDateTime localDateTime = LocalDateTime.now();
-//        LocalDateTime dateTimeVote = LocalDateTime.of(LocalDate.now().getYear(), LocalDateTime.now().getMonth(), LocalDateTime.now().getDayOfMonth(),
-//                10, 59, 59);
-//        return localDateTime.isBefore(dateTimeVote)
-//                ? new ResponseEntity<>(userService.vote(id), HttpStatus.ACCEPTED)
-//                : new ResponseEntity<>(userService.vote(id), HttpStatus.FORBIDDEN);
-//
-//    }
+    //Юзер отправляет Id ресторана - Ответ: id ресторана + дата
+    @PostMapping("/restaurants/{id}/vote")
+    public ResponseEntity<VoteResponse> vote(@PathVariable Long id) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDateTime dateTimeVote = LocalDateTime.of(LocalDate.now().getYear(), LocalDateTime.now().getMonth(), LocalDateTime.now().getDayOfMonth(),
+                10, 59, 59);
+        return localDateTime.isBefore(dateTimeVote)
+                ? new ResponseEntity<>(userService.vote(id), HttpStatus.ACCEPTED)
+                : new ResponseEntity<>(userService.vote(id), HttpStatus.FORBIDDEN);
+
+    }
 }
--- src/main/java/com/egorkuban/restaurantvote/service/UserService.java
+++ src/main/java/com/egorkuban/restaurantvote/service/UserService.java
@@ -1,11 +1,15 @@
 package com.egorkuban.restaurantvote.service;
 
 import com.egorkuban.restaurantvote.jpa.entity.RestaurantEntity;
+import com.egorkuban.restaurantvote.jpa.entity.VoteEntity;
 import com.egorkuban.restaurantvote.jpa.repository.RestaurantRepository;
+import com.egorkuban.restaurantvote.jpa.repository.UserRepository;
 import com.egorkuban.restaurantvote.jpa.repository.VoteRepository;
+import com.egorkuban.restaurantvote.model.response.VoteResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -13,9 +17,24 @@ import java.util.List;
 public class UserService {
     private final RestaurantRepository restaurantRepository;
     private final VoteRepository voteRepository;
+    private final UserRepository userRepository;
 
     public List<RestaurantEntity> getAllRestaurants() {
         return restaurantRepository.findAll();
     }
+
+    public VoteResponse vote(Long id) {
+        VoteEntity vote = new VoteEntity()
+                .setVoteDate(LocalDateTime.now())
+                .setRestaurant(restaurantRepository.findById(id)
+                        .orElseThrow(() -> new IllegalArgumentException("Restaurant not found by Id " + id)))
+                .setUserEntity(null);
+        return new VoteResponse()
+                .setRestaurantId(restaurantRepository.findById(id)
+                        .orElseThrow(() -> new IllegalArgumentException("Restaurant not found by Id " + id))
+                        .getId())
+                .setVoteDate(LocalDateTime.now());
+    }
+
 
 }
