bf987d17f4ad71f975b8e610d67993027206bda8
Egor Marchenko <t469mo@gmail.com>
2021-12-27 16:06:56 +0300

    Create Junit Test

--- src/test/java/com/egorkuban/restaurantvote/service/AdminServiceTest.java
+++ src/test/java/com/egorkuban/restaurantvote/service/AdminServiceTest.java
@@ -2,53 +2,104 @@ package com.egorkuban.restaurantvote.service;
 
 import com.egorkuban.restaurantvote.jpa.entity.RestaurantEntity;
 import com.egorkuban.restaurantvote.jpa.repository.RestaurantRepository;
+import com.egorkuban.restaurantvote.mapper.MealMapper;
 import com.egorkuban.restaurantvote.mapper.RestaurantMapper;
+import com.egorkuban.restaurantvote.model.MealDto;
+import com.egorkuban.restaurantvote.model.request.CreateMealRequest;
 import com.egorkuban.restaurantvote.model.request.CreateRestaurantRequest;
+import com.egorkuban.restaurantvote.model.response.CreatMealResponse;
 import com.egorkuban.restaurantvote.model.response.CreateRestaurantResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.stubbing.Answer;
 
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
 class AdminServiceTest {
-    @Autowired
     AdminService adminService;
-    @MockBean
+    RestaurantMapper restaurantMapper;
+    MealMapper mealMapper;
     RestaurantRepository restaurantRepository;
-    @MockBean
-    RestaurantMapper mapper;
 
+    @BeforeEach
+    public void init() {
+        mealMapper = new MealMapper();
+        restaurantMapper = new RestaurantMapper(mealMapper);
+        restaurantRepository = mock(RestaurantRepository.class);
+        adminService = new AdminService(restaurantRepository, restaurantMapper, mealMapper);
+    }
 
     @Test
     void createRestaurantTest() {
+        //prepare data
         CreateRestaurantRequest request = new CreateRestaurantRequest();
-        request.setName(anyString());
-        request.setAddress(anyString());
+        request.setName("restaurant_1");
+        request.setAddress("address_1");
 
+        //prepare mocks
+        when(restaurantRepository.save(any(RestaurantEntity.class)))
+                .then((Answer<RestaurantEntity>) invocationOnMock -> {
+                    RestaurantEntity arg = invocationOnMock.getArgument(0);
+                    arg.setId(1L);
+                    return arg;
+                });
+
+        //invoke
         CreateRestaurantResponse response = adminService.createRestaurant(request);
-        RestaurantEntity restaurant = new RestaurantEntity();
-        restaurant.setName(request.getName());
-        restaurant.setAddress(request.getAddress());
-        when(restaurantRepository.save(restaurant)).thenReturn(restaurantRepository.getById(restaurant.getId()));
-        when(mapper.mapToRestaurantDto(restaurant)).thenReturn(response.getRestaurantDto());
-        assertInstanceOf(CreateRestaurantResponse.class, response);
-        
 
+        //verify
+        verify(restaurantRepository, times(1)).save(any(RestaurantEntity.class));
+        assertEquals(response.getRestaurantDto().getId(), 1L);
+        assertEquals(response.getRestaurantDto().getName(), "restaurant_1");
+        assertEquals(response.getRestaurantDto().getAddress(), "address_1");
     }
 
     @Test
-    void deleteRestaurant() {
+    void deleteRestaurantTest() {
+        RestaurantEntity entity = new RestaurantEntity()
+                .setId(1L);
+        when(restaurantRepository.findById(entity.getId())).thenReturn(Optional.of(entity)).thenReturn(Optional.empty());
+        adminService.deleteRestaurant(1L);
+        verify(restaurantRepository, times(1)).delete(any(RestaurantEntity.class));
     }
 
     @Test
-    void createMeals() {
+    void createMealsTest() {
+        List<MealDto> meals = List.of(
+                new MealDto()
+                        .setName("MealName1")
+                        .setPrice(BigDecimal.valueOf(100)),
+                new MealDto()
+                        .setName("MealName1")
+                        .setPrice(BigDecimal.valueOf(150))
+        );
+        CreateMealRequest request = new CreateMealRequest();
+        request.setMeals(meals);
+
+        RestaurantEntity entity = new RestaurantEntity()
+                .setId(1L)
+                .setName("restaurant_1")
+                .setAddress("address_1")
+                .setMeals(mealMapper.mapToMealsEntity(request.getMeals()));
+
+        when(restaurantRepository.findById(entity.getId())).thenReturn(Optional.of(entity)).thenReturn(Optional.empty());
+
+
+        CreatMealResponse response = adminService.createMeals(request, entity.getId());
+        verify(restaurantRepository, times(1)).save(any(RestaurantEntity.class));
+        assertEquals(response.getRestaurantDto().getId(), 1L);
+        assertEquals(response.getRestaurantDto().getName(), "restaurant_1");
+        assertEquals(response.getRestaurantDto().getAddress(), "address_1");
+        assertNotNull(response.getRestaurantDto().getMeals());
+
+
     }
 }
