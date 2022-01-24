ad7885510f65d3e7ee9f4c837b8619ce3ebd46b5
Egor Marchenko <t469mo@gmail.com>
2022-01-24 10:19:41 +0300

    no tests

--- src/test/java/com/egorkuban/restaurantvote/service/RestaurantServiceTest.java
+++ src/test/java/com/egorkuban/restaurantvote/service/RestaurantServiceTest.java
@@ -1,113 +1,113 @@
-package com.egorkuban.restaurantvote.service;
-
-import com.egorkuban.restaurantvote.jpa.model.Dish;
-import com.egorkuban.restaurantvote.jpa.model.Menu;
-import com.egorkuban.restaurantvote.jpa.model.Restaurant;
-import com.egorkuban.restaurantvote.repository.MealRepository;
-import com.egorkuban.restaurantvote.repository.MenuRepository;
-import com.egorkuban.restaurantvote.repository.RestaurantRepository;
-import com.egorkuban.restaurantvote.to.RestaurantDto;
-import com.egorkuban.restaurantvote.to.request.CreateRestaurantRequest;
-import com.egorkuban.restaurantvote.to.response.CreateRestaurantResponse;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.stubbing.Answer;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-
-
-class RestaurantServiceTest {
-    RestaurantService restaurantService;
-    MenuService mealService;
-    RestaurantRepository restaurantRepository;
-    MealRepository mealRepository;
-    MenuRepository menuRepository;
-
-    @BeforeEach
-    public void init() {
-        restaurantRepository = mock(RestaurantRepository.class);
-        mealRepository = mock(MealRepository.class);
-        menuRepository = mock(MenuRepository.class);
-        restaurantService = new RestaurantService(restaurantRepository);
-        mealService = new MenuService(restaurantRepository,menuRepository);
-    }
-
-    @Test
-    void createRestaurantTest() {
-        CreateRestaurantRequest request = new CreateRestaurantRequest();
-        request.setName("restaurant_1");
-        request.setAddress("address_1");
-
-        when(restaurantRepository.save(any(Restaurant.class)))
-                .then((Answer<Restaurant>) invocationOnMock -> {
-                    Restaurant arg = invocationOnMock.getArgument(0);
-                    arg.setId(1L);
-                    return arg;
-                });
-
-        CreateRestaurantResponse response = restaurantService.createRestaurant(request);
-
-        verify(restaurantRepository, times(1)).save(any(Restaurant.class));
-        assertEquals(response.getRestaurantDto().getId(), 1L);
-        assertEquals(response.getRestaurantDto().getName(), "restaurant_1");
-        assertEquals(response.getRestaurantDto().getAddress(), "address_1");
-    }
-
-    @Test
-    void deleteRestaurantTest() {
-        Restaurant restaurant = new Restaurant()
-                .setId(1L);
-        when(restaurantRepository.findById(eq(1L))).thenReturn(Optional.of(restaurant));
-        restaurantService.deleteRestaurant(1L);
-        verify(restaurantRepository, times(1)).delete(eq(restaurant));
-    }
-    @Test
-    void getAllRestaurantsTest() {
-        Restaurant restaurant = new Restaurant();
-        restaurant.setName("NameRestaurant");
-        restaurant.setAddress("AddressRestaurant");
-        restaurant.setId(1L);
-
-        List<Dish> dishes = new ArrayList<>(Arrays.asList(
-                new Dish()
-                        .setName("mealName")
-                        .setId(1L)
-                        .setPrice(BigDecimal.valueOf(500)),
-                new Dish()
-                        .setName("mealName1")
-                        .setId(2L)
-                        .setPrice(BigDecimal.valueOf(400))
-        ));
-        List<Menu> menus = new ArrayList<>();
-        Menu menu = new Menu();
-        menu.setRestaurant(restaurant);
-        menu.setDate(LocalDate.now());
-        menu.setDishes(dishes);
-        menus.add(menu);
-
-
-        restaurant.setDishes(menus);
-
-        List<Restaurant> restaurantsList = new ArrayList<>();
-        restaurantsList.add(restaurant);
-
-        when(restaurantRepository.findAll()).thenReturn(restaurantsList);
-
-        List<RestaurantDto> restaurants = restaurantService.getAllRestaurants();
-
-        assertEquals(restaurants.size(),1);
-        assertEquals(restaurants.get(0).getName(),"NameRestaurant");
-        assertEquals(restaurants.get(0).getId(),1L);
-    }
-
-
-}
+//package com.egorkuban.restaurantvote.service;
+//
+//import com.egorkuban.restaurantvote.jpa.model.Dish;
+//import com.egorkuban.restaurantvote.jpa.model.Menu;
+//import com.egorkuban.restaurantvote.jpa.model.Restaurant;
+//import com.egorkuban.restaurantvote.repository.MealRepository;
+//import com.egorkuban.restaurantvote.repository.MenuRepository;
+//import com.egorkuban.restaurantvote.repository.RestaurantRepository;
+//import com.egorkuban.restaurantvote.to.RestaurantDto;
+//import com.egorkuban.restaurantvote.to.request.CreateRestaurantRequest;
+//import com.egorkuban.restaurantvote.to.response.CreateRestaurantResponse;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.stubbing.Answer;
+//
+//import java.math.BigDecimal;
+//import java.time.LocalDate;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.Optional;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.Mockito.*;
+//
+//
+//class RestaurantServiceTest {
+//    RestaurantService restaurantService;
+//    MenuService mealService;
+//    RestaurantRepository restaurantRepository;
+//    MealRepository mealRepository;
+//    MenuRepository menuRepository;
+//
+//    @BeforeEach
+//    public void init() {
+//        restaurantRepository = mock(RestaurantRepository.class);
+//        mealRepository = mock(MealRepository.class);
+//        menuRepository = mock(MenuRepository.class);
+//        restaurantService = new RestaurantService(restaurantRepository);
+//        mealService = new MenuService(restaurantRepository,menuRepository);
+//    }
+//
+//    @Test
+//    void createRestaurantTest() {
+//        CreateRestaurantRequest request = new CreateRestaurantRequest();
+//        request.setName("restaurant_1");
+//        request.setAddress("address_1");
+//
+//        when(restaurantRepository.save(any(Restaurant.class)))
+//                .then((Answer<Restaurant>) invocationOnMock -> {
+//                    Restaurant arg = invocationOnMock.getArgument(0);
+//                    arg.setId(1L);
+//                    return arg;
+//                });
+//
+//        CreateRestaurantResponse response = restaurantService.createRestaurant(request);
+//
+//        verify(restaurantRepository, times(1)).save(any(Restaurant.class));
+//        assertEquals(response.getRestaurantDto().getId(), 1L);
+//        assertEquals(response.getRestaurantDto().getName(), "restaurant_1");
+//        assertEquals(response.getRestaurantDto().getAddress(), "address_1");
+//    }
+//
+//    @Test
+//    void deleteRestaurantTest() {
+//        Restaurant restaurant = new Restaurant()
+//                .setId(1L);
+//        when(restaurantRepository.findById(eq(1L))).thenReturn(Optional.of(restaurant));
+//        restaurantService.deleteRestaurant(1L);
+//        verify(restaurantRepository, times(1)).delete(eq(restaurant));
+//    }
+//    @Test
+//    void getAllRestaurantsTest() {
+//        Restaurant restaurant = new Restaurant();
+//        restaurant.setName("NameRestaurant");
+//        restaurant.setAddress("AddressRestaurant");
+//        restaurant.setId(1L);
+//
+//        List<Dish> dishes = new ArrayList<>(Arrays.asList(
+//                new Dish()
+//                        .setName("mealName")
+//                        .setId(1L)
+//                        .setPrice(BigDecimal.valueOf(500)),
+//                new Dish()
+//                        .setName("mealName1")
+//                        .setId(2L)
+//                        .setPrice(BigDecimal.valueOf(400))
+//        ));
+//        List<Menu> menus = new ArrayList<>();
+//        Menu menu = new Menu();
+//        menu.setRestaurant(restaurant);
+//        menu.setDate(LocalDate.now());
+//        menu.setDishes(dishes);
+//        menus.add(menu);
+//
+//
+//        restaurant.setDishes(menus);
+//
+//        List<Restaurant> restaurantsList = new ArrayList<>();
+//        restaurantsList.add(restaurant);
+//
+//        when(restaurantRepository.findAll()).thenReturn(restaurantsList);
+//
+//        List<RestaurantDto> restaurants = restaurantService.getAllRestaurants();
+//
+//        assertEquals(restaurants.size(),1);
+//        assertEquals(restaurants.get(0).getName(),"NameRestaurant");
+//        assertEquals(restaurants.get(0).getId(),1L);
+//    }
+//
+//
+//}
--- src/test/java/com/egorkuban/restaurantvote/service/UserServiceTest.java
+++ src/test/java/com/egorkuban/restaurantvote/service/UserServiceTest.java
@@ -1,86 +1,86 @@
-package com.egorkuban.restaurantvote.service;
-
-import com.egorkuban.restaurantvote.jpa.model.Restaurant;
-import com.egorkuban.restaurantvote.jpa.model.User;
-import com.egorkuban.restaurantvote.jpa.model.Vote;
-import com.egorkuban.restaurantvote.repository.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.time.LocalDate;
-import java.util.Optional;
-
-import static org.mockito.Mockito.*;
-
-class UserServiceTest {
-    RestaurantService restaurantService;
-    MenuService menuService;
-    VoteService voteService;
-    UserService userService;
-    RestaurantRepository restaurantRepository;
-    MealRepository mealRepository;
-    VoteRepository voteRepository;
-    UserRepository userRepository;
-    MenuRepository menuRepository;
-    @BeforeEach
-    public void init() {
-        restaurantRepository = mock(RestaurantRepository.class);
-        voteRepository = mock(VoteRepository.class);
-        userRepository = mock(UserRepository.class);
-        mealRepository = mock(MealRepository.class);
-        menuRepository = mock(MenuRepository.class);
-        userService = mock(UserService.class);
-        voteService = spy(new VoteService(userService, voteRepository,restaurantRepository,userRepository));
-        restaurantService = new RestaurantService(restaurantRepository);
-        menuService = new MenuService(restaurantRepository,menuRepository);
-    }
-
-
-
-    @Test
-    void voteTest() {
-
-        Restaurant oldVoteRestaurant = new Restaurant()
-                .setName("Name_Restaurant")
-                .setId(1L);
-
-        User user = new User();
-        user.setEmail("Example@email.com");
-        user.setId(1L);
-
-        Vote vote = new Vote();
-        vote.setVoteDate(LocalDate.now());
-        vote.setRestaurant(oldVoteRestaurant);
-        vote.setUser(user);
-
-        Restaurant newVoteRestaurant = new Restaurant()
-                .setName("New_Restaurant")
-                .setId(2L);
-
-        when(voteRepository.findByVoteDateAndUserId(LocalDate.now(), 1L)).thenReturn(Optional.of(vote));
-        when(restaurantRepository.getById(eq(2L))).thenReturn(newVoteRestaurant);
-        when(voteService.isTimeExpired()).thenReturn(false);
-
-        voteService.vote(2L);
-
-        verify(voteRepository).save(argThat((Vote v ) -> v.getUser().getId() == 1L));
-    }
-
-    @Test
-    void voteTestNewVote() {
-        User user = new User();
-        user.setEmail("Example@email.com");
-        user.setId(1L);
-
-        Restaurant restaurant = new Restaurant()
-                .setName("Name_Restaurant")
-                .setId(1L);
-
-        when(restaurantRepository.getById(eq(1L))).thenReturn(restaurant);
-        when(userRepository.getById(eq(1L))).thenReturn(user);
-
-        voteService.vote(1L);
-
-        verify(voteRepository).save(argThat((Vote vote ) -> vote.getUser().getId() == 1L));
-    }
-}
+//package com.egorkuban.restaurantvote.service;
+//
+//import com.egorkuban.restaurantvote.jpa.model.Restaurant;
+//import com.egorkuban.restaurantvote.jpa.model.User;
+//import com.egorkuban.restaurantvote.jpa.model.Vote;
+//import com.egorkuban.restaurantvote.repository.*;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//import java.time.LocalDate;
+//import java.util.Optional;
+//
+//import static org.mockito.Mockito.*;
+//
+//class UserServiceTest {
+//    RestaurantService restaurantService;
+//    MenuService menuService;
+//    VoteService voteService;
+//    UserService userService;
+//    RestaurantRepository restaurantRepository;
+//    MealRepository mealRepository;
+//    VoteRepository voteRepository;
+//    UserRepository userRepository;
+//    MenuRepository menuRepository;
+//    @BeforeEach
+//    public void init() {
+//        restaurantRepository = mock(RestaurantRepository.class);
+//        voteRepository = mock(VoteRepository.class);
+//        userRepository = mock(UserRepository.class);
+//        mealRepository = mock(MealRepository.class);
+//        menuRepository = mock(MenuRepository.class);
+//        userService = mock(UserService.class);
+//        voteService = spy(new VoteService(userService, voteRepository,restaurantRepository,userRepository));
+//        restaurantService = new RestaurantService(restaurantRepository);
+//        menuService = new MenuService(restaurantRepository,menuRepository);
+//    }
+//
+//
+//
+//    @Test
+//    void voteTest() {
+//
+//        Restaurant oldVoteRestaurant = new Restaurant()
+//                .setName("Name_Restaurant")
+//                .setId(1L);
+//
+//        User user = new User();
+//        user.setEmail("Example@email.com");
+//        user.setId(1L);
+//
+//        Vote vote = new Vote();
+//        vote.setVoteDate(LocalDate.now());
+//        vote.setRestaurant(oldVoteRestaurant);
+//        vote.setUser(user);
+//
+//        Restaurant newVoteRestaurant = new Restaurant()
+//                .setName("New_Restaurant")
+//                .setId(2L);
+//
+//        when(voteRepository.findByVoteDateAndUserId(LocalDate.now(), 1L)).thenReturn(Optional.of(vote));
+//        when(restaurantRepository.getById(eq(2L))).thenReturn(newVoteRestaurant);
+//        when(voteService.isTimeExpired()).thenReturn(false);
+//
+//        voteService.vote(2L);
+//
+//        verify(voteRepository).save(argThat((Vote v ) -> v.getUser().getId() == 1L));
+//    }
+//
+//    @Test
+//    void voteTestNewVote() {
+//        User user = new User();
+//        user.setEmail("Example@email.com");
+//        user.setId(1L);
+//
+//        Restaurant restaurant = new Restaurant()
+//                .setName("Name_Restaurant")
+//                .setId(1L);
+//
+//        when(restaurantRepository.getById(eq(1L))).thenReturn(restaurant);
+//        when(userRepository.getById(eq(1L))).thenReturn(user);
+//
+//        voteService.vote(1L);
+//
+//        verify(voteRepository).save(argThat((Vote vote ) -> vote.getUser().getId() == 1L));
+//    }
+//}
